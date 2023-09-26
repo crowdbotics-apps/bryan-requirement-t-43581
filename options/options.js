a0492685222c20b27249550432a118c89fef360b
Crowdbotics <team@crowdbotics.com>
2023-09-26 19:26:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bryan-requirement-t-43581",
+  url: "https://bryan-requirement-t-43581.botics.co",
+  api: "https://bryan-requirement-t-43581.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
